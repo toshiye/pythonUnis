f01432ffb996a3208dcc24f25f8272943de33f98
toshiye <sasakikenji@gmail.com>
2019-11-04 10:29:02 -0300

    20191103_morning

--- ciclo2/numeroPrimo.py
+++ ciclo2/numeroPrimo.py
@@ -1,57 +1,17 @@
-"""for n in range(1, 101):
-    if n / 1 == n and n / n == 1:
-        print ('O número %d é primo' %(n))"""
+num = int(input('Digite um número:'))
 
-"""for n in range(1, 101):
-    if n%2 == 1:
-        print ('O número %d é primo' %(n))"""
+tot = 0
 
-"""for n in range(2, 101):
-    if 101 % n :
-        print ('O número %d é primo' %(n))"""
-
-"""num = 1
-topo = 100
-while num <= topo:
-    if num%2 == 1:
-        num = num + 1
-        print ('O número %d é primo' %(num))"""
-
-"""num = 1
-topo = 100
-while num <= topo:
-    if num != 1 or num % 2 != 0:
-        num = num + 1
-        print ('O número %d é primo' %(num))"""
-
-"""numeros = []
-
-for i in range(1, 100):
-    numeros.append(i+1)
-for n in numeros:
-    if numeros % n == 0:
-        print('O número %d é Primo!' %(n))
+for c in range(1, num+1):
+    if num % c == 0:
+        print('\33[34m')
+        tot += 1
     else:
-        print('O número %d não é Primo!' %(n))"""
+        print('\33[m')
+    print ('{} '.format(c), end='')
+print('\n\33[mO número {} foi divisível {} vezes.'.format(num, tot))
 
-"""numeros = range(1, 100)
-for n in numeros:
-    if numeros % n == 0:
-        print('O número %d é Primo!' %(n))
-    else:
-        print('O número %d não é Primo!' %(n))"""
-
-def primeNumber(numero):
-    divisoes = 0
-    for c in range(1, numero+1):
-        if numero % c == 0:
-            divisoes += 1
-        if divisoes == 2:
-            print('O número %d é Primo!' %(c))
-        else:
-            print('O número %d não é Primo!' %(c))
-
-numbers = range(1, 100+1)
-
-for n in numbers:
-    primeNumber(n)
+if tot == 2:
+    print('E por isso ele é Primo!')
+else:
+    print('E por isso ele não é Primo!')
